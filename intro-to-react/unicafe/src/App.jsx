1cf0c4cf1f5411591fe930b8d2e6a288624fb1d8
BrandonDrayton <brandondrayton@gmail.com>
2023-10-05 19:17:01 -0400

    Refactored for improved usability

--- intro-to-react/unicafe/src/App.jsx
+++ intro-to-react/unicafe/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import React, { useState } from "react";
+
 const Buttons = ({ onBadClick, onGoodClick, onNeutralClick }) => {
   return (
     <div>
@@ -12,54 +13,53 @@ const Buttons = ({ onBadClick, onGoodClick, onNeutralClick }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <tbody>
-      <tr>
-        <td>{text}:</td>
-        <td>{value}</td>
-      </tr>
-    </tbody>
+    <tr>
+      <td>{text}:</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
 const Statistics = ({ good, neutral, bad, sum, avg, positive }) => {
+  const lines = [
+    { text: "Good", value: good },
+    { text: "Neutral", value: neutral },
+    { text: "Bad", value: bad },
+    { text: "All", value: sum },
+    { text: "Average", value: avg },
+    { text: "Positive", value: positive },
+  ];
+
   return (
     <table>
-      <StatisticLine text="Good" value={good} />
-      <StatisticLine text="Neutral" value={neutral} />
-      <StatisticLine text="Bad" value={bad} />
-      <StatisticLine text="All" value={sum} />
-      <StatisticLine text="Average" value={avg} />
-      <StatisticLine text="Positive" value={good} />
+      <tbody>
+        {lines.map((line, index) => (
+          <StatisticLine key={index} text={line.text} value={line.value} />
+        ))}
+      </tbody>
     </table>
   );
 };
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const handleGoodClick = () => {
-    setGood(good + 1);
-  };
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1);
-  };
-  const handleBadClick = () => {
-    setBad(bad + 1);
+  const handleClick = (setter) => () => {
+    setter((prev) => prev + 1);
   };
 
   const sum = good + neutral + bad;
   const avg = sum === 0 ? 0 : (good - bad) / sum;
-  const positive = sum === 0 ? 0 : good / sum;
+  const positive = sum === 0 ? 0 : (good / sum) * 100;
 
   return (
     <div>
       <Buttons
-        onBadClick={handleBadClick}
-        onGoodClick={handleGoodClick}
-        onNeutralClick={handleNeutralClick}
+        onBadClick={handleClick(setBad)}
+        onGoodClick={handleClick(setGood)}
+        onNeutralClick={handleClick(setNeutral)}
       />
       <div>
         <h2>Statistics</h2>
@@ -71,8 +71,8 @@ const App = () => {
             bad={bad}
             neutral={neutral}
             sum={sum}
-            avg={avg}
-            positive={positive}
+            avg={avg.toFixed(2)}
+            positive={positive.toFixed(2) + "%"}
           />
         )}
       </div>
