e417200bbb15cf72698683de315be9eae318ecf5
BrandonDrayton <brandondrayton@gmail.com>
2023-10-10 19:47:19 -0400

    Added header component/testing array

--- communicating-with server/course-information/Components/Course.jsx
+++ communicating-with server/course-information/Components/Course.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Course = () => {
-  return <div>Course</div>;
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header title={course.name} />
+    </>
+  );
+};
+const Header = ({ title }) => {
+  return <h1>{title}</h1>;
 };
 
 export default Course;
