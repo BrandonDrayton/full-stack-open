4867796bfa8ac058608b651922045ef235523e3e
BrandonDrayton <brandondrayton@gmail.com>
2023-10-10 20:15:32 -0400

    Content component added

--- communicating-with server/course-information/Components/Course.jsx
+++ communicating-with server/course-information/Components/Course.jsx
@@ -4,6 +4,7 @@ const Course = ({ course }) => {
   return (
     <>
       <Header title={course.name} />
+      <Content parts={course.parts} />
     </>
   );
 };
@@ -11,4 +12,21 @@ const Header = ({ title }) => {
   return <h1>{title}</h1>;
 };
 
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <div
+          key={part.id}
+          text={part.name}
+          value={part.exercises}
+          {...console.log(part.id)}
+        >
+          {console.log(part.name)}
+          {part.name}: {part.exercises}
+        </div>
+      ))}
+    </>
+  );
+};
 export default Course;
